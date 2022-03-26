cd59d4f39c79bd429cfd824814bbc863817a1f97
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-26 17:54:08 -0400

    complete next auth module

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 
 import classes from './auth-form.module.css';
 
@@ -24,6 +25,8 @@ function AuthForm() {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
+  const router = useRouter();
+
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
@@ -41,7 +44,10 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword,
       });
-      console.log(result);
+
+      if (!result.error) {
+        router.replace('/profile');
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,13 +1,9 @@
-import { getSession } from 'next-auth/client';
-import { useEffect, useState } from 'react';
-
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  // Client side page Guards
   // useEffect(() => {
   //   getSession().then((session) => {
   //     if (!session) {
@@ -22,10 +18,24 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
