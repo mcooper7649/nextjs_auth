09884ee317de0c65c30d4445572b48f0ddbfcf56
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-26 13:07:07 -0400

    added boilerplate, bcrypt, userauth and db is functional

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,37 @@
+import { connectToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message: 'Invalid input - password should also be at least 7 characters',
+    });
+    return;
+  }
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'Created User' });
+  console.log(result);
+}
+
+export default handler;
