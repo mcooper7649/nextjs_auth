9920bc58dd447c906bfe4b9cf16d20a82e1ddc12
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-26 16:45:04 -0400

    added server side pag guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -7,19 +7,20 @@ import classes from './user-profile.module.css';
 function UserProfile() {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  // Client side page Guards
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
